c0e7cda0919eeb7a96bd51d99c3e23ff432b534c
jobatake <jobatake@hawaii.edu>
2016-05-11 16:50:17 -1000

    Update sensors.cpp

--- sensors.cpp
+++ sensors.cpp
@@ -1,20 +1,52 @@
-/* Team Quaser *
+/* Team Quasar *
  * sensors.cpp */
 
 /* Libraries used */
 
 #include "sensors.h"
 #include "config.h"
+#include <SoftwareSerial.h>
 
-/* Set Pins */
+/* Sensor object variables */
 Adafruit_BMP085 bmp;
 SHT1x sht1x(HUMID_DATA, HUMID_CLK);
-OneWire ds(ROOF_TEMP);
+OneWire onewire(ROOF_TEMP);
+DallasTemperature roof_temp(&onewire);
 
 /* Function intializer */
-
 void init1(void){
-   bmp.begin();
+   bmp.begin(); //Pressure initialization
+}
+
+/*----------------*/
+/*------Battery------*/
+/*----------------*/
+uint16_t battery(void)
+{
+    int16_t battery;
+    
+    battery = analogRead(BATTERY)*5000.0/1023;
+
+    /* Debug Battery */
+    Serial.print("Battery");
+    Serial.println(battery);
+    
+    return battery;
+}
+
+/*----------------*/
+/*------Panel------*/
+/*----------------*/
+uint16_t panel(void)
+{
+    int16_t panel;
+    
+    panel = 2*analogRead(PANEL)*5000.0/1023;
+    
+    /* Debug Panel */
+    Serial.print("Panel");
+    Serial.println(panel);
+    return panel;
 }
 
 /*----------------*/
@@ -22,64 +54,17 @@ void init1(void){
 /*----------------*/
 
 int16_t temp(void)
- {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  int16_t celsius, fahrenheit;
+{   int16_t celsius = 0;
+
+    //celsius =  bmp.readTemperature(); //bmp085 pressure sensor temp reading
     
-  if (!ds.search(addr)) {
-      Serial.println();
-      ds.reset_search();
-  }
+    roof_temp.requestTemperatures();
+    celsius = roof_temp.getTempCByIndex(0);
 
- if (OneWire::crc8(addr, 7) != addr[7]) {
- Serial.println("No Temp Address");
-
- }
- ds.reset();
- ds.select(addr);
- ds.write(0x44, 1);
-
- present = ds.reset();
- ds.select(addr);    
- ds.write(0xBE);
-
- for ( i = 0; i < 9; i++) {           // we need 9 bytes
-     data[i] = ds.read();
-     }
-
-/* Convert the data to actual temperature */
- int16_t raw = (data[1] << 8) | data[0];
- if (type_s) {
-/* 9 bit resolution default */
- raw = raw << 3;
-/* gives full12 bit resolution */
- if (data[7] == 0x10) {
- raw = (raw & 0xFFF0) + 12 - data[6];
- }
-
-} else {
-      byte cfg = (data[4] & 0x60);
- /* At lower res, the low bits are undefined, so let's zero them */
-  if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-  else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-  else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
- }
-
-/* Returns data */
-
-celsius = (int16_t)raw / 16.0;
-//fahrenheit = celsius * 1.8 + 32.0;
-
-/* Debug Temp */
-//Serial.print("Temp");
-//Serial.println(celsius);
- 
-return celsius;
-//return fahrenheit;
+    /* Debug Temp */
+    Serial.print("Temp");
+    Serial.println(celsius);
+    return celsius;
 }
 
 /*-----------------*/
@@ -87,11 +72,11 @@ return celsius;
 /*-----------------*/
 
 uint16_t humidity(void){
-uint16_t humidityper = sht1x.readHumidity();
+    uint16_t humidityper = sht1x.readHumidity();
 
-/* Debug Humidity */
-//Serial.print("Humidity");
-//Serial.println(humidityper);
+    /* Debug Humidity */
+    Serial.print("Humidity");
+    Serial.println(humidityper);
  
 return humidityper;
 }
@@ -100,14 +85,14 @@ return humidityper;
 /*----Pressure----*/
 /*----------------*/
 
-int32_t pressure(void){
-int32_t pressurePa = bmp.readPressure();
+uint32_t pressure(void){
+    uint32_t pressurePa = bmp.readPressure();
 
-/* Debug Pressure */
-//Serial.print("Pressure");
-//Serial.println(pressurePa);
+    /* Debug Pressure */
+    Serial.print("Pressure");
+    Serial.println(pressurePa);
 
-return pressurePa;
+    return pressurePa;
 }
 
 /*----------------*/
@@ -115,11 +100,11 @@ return pressurePa;
 /*----------------*/
 
 uint16_t irradiance(void){
-uint16_t irradiancemV = analogRead(APOGEE_SOLAR)*5000.0/1023;
+    uint16_t irradiancemV = analogRead(APOGEE_SOLAR)*5000.0/1023;
 
-/* Debug Irradiance */
-//Serial.print("Solar Irradiance");
-//Serial.println(irradiancemV);
+    /* Debug Irradiance */
+    Serial.print("Solar Irradiance");
+    Serial.println(irradiancemV);
 
-return irradiancemV;
+    return irradiancemV;
 }
